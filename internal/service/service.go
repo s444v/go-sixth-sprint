592c3043aac70d0955d9adda28320d3cee0eec70
s444v <dymnich02@gmail.com>
2025-04-10 09:56:42 +0300

    try

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,13 @@
 package service
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func stringDetector(s string) string {
+	if check := regexp.MustCompile(`.-`); check.MatchString(s) {
+		fmt.Println(true)
+	}
+	fmt.Println(false)
+}
